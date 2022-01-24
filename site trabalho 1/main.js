72fc83b56978f5e4f7099e7bb0d6262cfa84f215
oda <95103267+odaaaaaa@users.noreply.github.com>
2022-01-23 21:51:46 -0300

    ss

--- site trabalho 1/main.js
+++
@@ -1,6 +0,0 @@
-const botao = document.querySelector('#botao')
-
-botao.addEventListener('click', () => {
-    window.scroll({top: window.innerHeight, behavior: "smooth"})
-    botao.style.display = 'none'
-})
